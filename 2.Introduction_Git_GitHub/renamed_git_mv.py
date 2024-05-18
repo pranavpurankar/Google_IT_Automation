8e3afdad3811d328a6afb86211b5577bd3f6504d
pranavpurankar <pranav.purankar@gmail.com>
2024-05-18 23:44:52 +0530

    deleted renamed_git_mv.py

--- 2.Introduction_Git_GitHub/renamed_git_mv.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def Check_power_usage():
-    print("Hello World")
-    print("I am learning about the programming")
-    print("Spped up pranav")
-    print("Try to write long programs also create an array data structure from scratch")
