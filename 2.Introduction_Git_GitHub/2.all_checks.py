d0e43c3ad63b4ddc0b2272f773bb8fe294a6aff0
pranavpurankar <pranav.purankar@gmail.com>
2024-05-20 10:48:04 +0530

    it should be os.path.exists

--- 2.Introduction_Git_GitHub/2.all_checks.py
+++ 2.Introduction_Git_GitHub/2.all_checks.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 def check_reboot():
-    '''Returns True if the computer has a pending reboot. '''
+    '''Returns True if the computer has a pending reboot'''
     return os.path.exists("/run/reboot-required")
 
 def main():
