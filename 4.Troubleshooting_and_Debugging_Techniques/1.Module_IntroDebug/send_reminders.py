4da6a5f496851be7b093eff899e3eb95c772d8d1
pranavpurankar <pranav.purankar@gmail.com>
2024-05-26 17:49:40 +0530

    debugging super useful concept

---
+++ 4.Troubleshooting_and_Debugging_Techniques/1.Module_IntroDebug/send_reminders.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+    if len(sys.argv) < 2:
+        return usage()
+
+    try:
+        date, title, emails = sys.argv[1].split('|')
+        message = message_template(date, title)
+        send_message(message, emails)
+        print("Successfully sent reminder to: ",emails)
+    except Exception as e:
+        print("Failure to send email", file=sys.stderr)
+    except Exception as e:
+        print("Failure to send email: {}".format(e), file=sys.stderr)
+
